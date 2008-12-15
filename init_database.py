fc89b91985d482e2524cac7d725cc973892a2ca8
tsing <tsing@jianqing.org>
2008-12-15 10:13:04 +0800

    Remove ridiculous lines

--- init_database.py
+++ init_database.py
@@ -4,8 +4,4 @@ sys.path.append('lib')
 import models
 from sqlalchemy import *
 
-col = Column('active_even_offline', Boolean, default=False);
-col.create(models._users_table)
-
-#models._metadata.drop_all(models._engine)
-#models._metadata.create_all(models._engine)
+models._metadata.create_all(models._engine)
